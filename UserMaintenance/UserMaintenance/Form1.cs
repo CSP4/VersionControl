ed921aa1d58d403c89d0f24804529cd8f01149fb
CSP CSP <violacsaba@gmail.com>
2020-09-27 12:48:43 +0200

    commit add datasource and new user

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -7,18 +7,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
             labelLastName.Text = Names.LastName;
             labelFirstName.Text = Names.FirstName;
             buttonAdd.Text = Names.Add;
+
+            listBoxNames.DataSource = users;
+            listBoxNames.ValueMember = "ID";
+            listBoxNames.DisplayMember = "FullName";
         }
 
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = textBoxLastName.Text,
+                FirstName = textBoxFirstName.Text
+            };
+            users.Add(u);
+        }
     }
 }
