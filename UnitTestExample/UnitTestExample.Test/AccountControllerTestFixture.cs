5ffa37baf6355da8854a81e046dbd2a83c7643af
CSP CSP <violacsaba@gmail.com>
2020-11-22 18:22:58 +0100

    Commit EmailValidateTest

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -8,20 +8,27 @@ using UnitTestExample.Controllers;
 
 namespace UnitTestExample.Test
 {
+
     public class AccountControllerTestFixture
     {
-        [Test]
+        [
+            Test,
+            TestCase("abcd1234", false),
+            TestCase("irf@uni-corvinus", false),
+            TestCase("irf.uni-corvinus.hu", false),
+            TestCase("irf@uni-corvinus.hu", true)
+        ]
         public void TestValidateEmail(string email, bool expectedResult)
         {
-            //Arrange
+            // Arrange
             var accountController = new AccountController();
 
-            //Act
+            // Act
             var actualResult = accountController.ValidateEmail(email);
 
-            //Assert
+            // Assert
             Assert.AreEqual(expectedResult, actualResult);
-            
+
         }
     }
 }
